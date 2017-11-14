8caa608d2cbe7c51acb8dfdf38945a4b5d351502
CNWeiYao <33653234+CNWeiYao@users.noreply.github.com>
2017-11-14 18:22:18 +0800

    Update MainActivity.java

--- Demo001/MainActivity.java
+++ Demo001/MainActivity.java
@@ -15,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
         draw.setMinimumWidth(500);
         draw.setMinimumHeight(300);
         show.addView(draw);
+        /*
+        实际上可以在XML中管理布局文件将DrawView组件加到LinearLayout组件上可省去13-17行，代码如下
+        <LinearLayout
+        android:id="@+id/root"
+        android:layout_width="368dp"
+        android:layout_height="500dp"
+        android:orientation="horizontal"
+        tools:layout_editor_absoluteX="8dp"
+        tools:layout_editor_absoluteY="8dp"
+        tools:ignore="MissingConstraints">
+    <com.example.root.demo001.DrawView
+        android:layout_height="match_parent"
+        android:layout_width="match_parent"></com.example.root.demo001.DrawView>
+    </LinearLayout>
+         */
     }
 }
